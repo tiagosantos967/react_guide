92d59ec653e947ab3af70783a6f53f3bb0084a75
Tiago <tiago.s.1189@gmail.com>
2018-12-02 15:19:00 +0100

    updated to work with redux

--- src/components/organisms/LoginBox/index.stories.js
+++ src/components/organisms/LoginBox/index.stories.js
@@ -3,7 +3,13 @@ import { storiesOf } from '@storybook/react';
 
 import { LoginBox } from 'components';
 
+//ref: https://medium.com/ingenious/storybook-meets-redux-6ab09a5be346
+import rootReducer from './reducer'
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux'
+const store = createStore(combineReducers({EditClient:rootReducer}))
 
 storiesOf('LoginBox', module)
+.addDecorator(story => <Provider store={store}>{story()}</Provider>)
 .add('default', () => (<LoginBox/>) )
 .add('error', () => (<LoginBox error/>) );
--- src/components/pages/LoginPage/index.stories.js
+++ src/components/pages/LoginPage/index.stories.js
@@ -3,6 +3,11 @@ import { storiesOf } from '@storybook/react';
 
 import { LoginPage } from 'components';
 
+//ref: https://medium.com/ingenious/storybook-meets-redux-6ab09a5be346
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux'
+const store = createStore(combineReducers({LoginPage:{}}))
 
 storiesOf('LoginPage', module)
+.addDecorator(story => <Provider store={store}>{story()}</Provider>)
 .add('default', () => (<LoginPage/>) );
